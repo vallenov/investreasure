f2abc96ae90cd260dd2b925b3dc7fdd996de3c73
vallenov <a2lenov@mail.ru>
2023-02-18 21:13:09 +0300

    Мелкие правки

--- investreasure/settings.py
+++ investreasure/settings.py
@@ -12,3 +12,14 @@ ALLOWED_HOSTS = ['*']
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 ROOT_URLCONF = 'urls'
+
+SECRET_KEY = getattr(local_settings, 'LOCAL_SECRET_KEY', '')
+
+STATIC_URL = '/static/'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'index',
+]
