7a7601e6e77f0865e90ac1b5dd166bbcdc49c692
vallenov <a2lenov@mail.ru>
2023-02-19 16:17:38 +0300

    Добавлены кастомные исключения

---
+++ investreasure/exceptions.py
@@ -0,0 +1,32 @@
+RESPONSE_CODES_MAPPING = {
+    'SUCCESS': 0,
+    'INTERNET_ERROR': 1,
+    'FILE_ERROR': 2,
+    'DB_ERROR': 3,
+    'CONFIG_ERROR': 4,
+    'HTTP_ERROR': 5,
+    'PARAMETERS_ERROR': 6,
+    'CACHE_ERROR': 7,
+    'MARKUP_ERROR': 8,
+    'UNKNOWN_ERROR': 100
+}
+
+
+class InvestreasureException(Exception):
+    """
+        Custom exceptions
+        Main params:
+        code - RESPONSE_CODES_MAPPING
+        message - error message for developer
+        return_message - message for user
+        """
+
+    CODES = RESPONSE_CODES_MAPPING
+
+    def __init__(self, *args, **kwargs):
+        self.context = {
+            'error_type': self.CODES[kwargs.get('type', 'UNKNOWN_ERROR')],
+        }
+        for key, value in kwargs.items():
+            self.context[key] = value
+        super().__init__(*args)
